c08dc43a3c414321401c58f47c15191853f5d1df
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-20 16:26:26 -0500

    Implementación de SideMenu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    Error404PageComponent
+    Error404PageComponent,
+    SideMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    SideMenuComponent
   ]
 })
 export class SharedModule { }
