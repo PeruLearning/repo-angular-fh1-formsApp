9e6324a3a81ad63742962590c3e56fd18f5d4a51
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-20 19:20:05 -0500

    Enlazando formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(''),
+  //   stock: new FormControl(0)
+  // });
+
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]]
+  });
+
+  public onSave(): void {
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+    this.myForm.reset({
+      price: -2
+    });
+  }
 }
